413c917bdea587e585617af018addcd2b14c1942
MateuszAmbrozy <ambrozy.mateusz@onet.pl>
2024-06-29 21:53:36 +0200

    add .

---
+++ WEB/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Authentication.Certificate;
+using WEB.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(CertificateAuthenticationDefaults.AuthenticationScheme).AddCertificate();
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
